08571170c4ef3cc384b858d8134c933ae0bcc68f
Andrey <semak16174@gmail.com>
2025-10-09 00:27:34 +0300

    task7

--- 3lab/lab3.py
+++ 3lab/lab3.py
@@ -107,3 +107,23 @@ def task6():
     
     return final_amount, profit
 task6()
+
+from fractions import Fraction
+def task7():
+    frac1 = Fraction(3, 4)
+    frac2 = Fraction(5, 6)
+    
+    print(f"Дробь 1: {frac1}")
+    print(f"Дробь 2: {frac2}")
+    
+    addition = frac1 + frac2
+    subtraction = frac1 - frac2
+    multiplication = frac1 * frac2
+    division = frac1 / frac2
+    
+    print("\nРезультаты операций:")
+    print(f"Сложение: {frac1} + {frac2} = {addition}")
+    print(f"Вычитание: {frac1} - {frac2} = {subtraction}")
+    print(f"Умножение: {frac1} * {frac2} = {multiplication}")
+    print(f"Деление: {frac1} / {frac2} = {division}")
+task7()
