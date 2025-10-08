90d1cc8997531c48c4c636d24a0a37f6a8ea1421
Andrey <semak16174@gmail.com>
2025-10-09 00:11:47 +0300

    task2

--- 3lab/lab3.py
+++ 3lab/lab3.py
@@ -4,3 +4,9 @@ def task1():
     print("Квадраты чисел от 1 до 10:", squares)
     return squares
 task1()
+
+def task2():
+    even_numbers = [x for x in range(1, 20) if x % 2 == 0]
+    print("Четные числа от 1 до 19:", even_numbers)
+    return even_numbers
+task2()
